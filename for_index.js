a51567b6be07766b4ae0edbccde9518af86cbbcb
dst111dst <72456015+dst111dst@users.noreply.github.com>
2021-07-06 02:48:23 +0800

    Create for_index.js

---
+++ for_index.js
@@ -0,0 +1,31 @@
+const searchBtn = document.getElementById('search-btn');
+const search = document.getElementById('query');
+const tip = document.getElementById('tip');
+
+var i = 0;
+var message = "find more..";
+var typeSpeed = 200;
+
+
+searchBtn.addEventListener('click', () => {
+  search.style.width = '80%';
+  search.style.paddingLeft = '60px';
+  search.style.cursor = 'text';
+  search.focus();
+
+  typeWriter();
+});
+
+search.addEventListener('keydown', () => {
+  tip.style.visibility = "visible";
+  tip.style.opacity = 1;
+});
+
+function typeWriter() {
+  if (i < message.length) {
+    msg = search.getAttribute('placeholder') + message.charAt(i);
+    search.setAttribute('placeholder', msg);
+    i++;
+    setTimeout(typeWriter, typeSpeed);
+  }
+}
