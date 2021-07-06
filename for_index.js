7d3cce78098d1f2fd33162d5857e750e8d6a060f
dst111dst <72456015+dst111dst@users.noreply.github.com>
2021-07-06 16:33:35 +0800

    Update for_index.js

--- for_index.js
+++ for_index.js
@@ -3,7 +3,7 @@ const search = document.getElementById('query');
 const tip = document.getElementById('tip');
 
 var i = 0;
-var message = “...";
+var message = "this is a demo..."
 var typeSpeed = 200;
 
 
