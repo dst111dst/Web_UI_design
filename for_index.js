8e636b3d301bd8289beb0852cd399478a8b4a7c6
dst111dst <72456015+dst111dst@users.noreply.github.com>
2021-07-07 01:10:05 +0800

    Update for_index.js

--- for_index.js
+++ for_index.js
@@ -3,7 +3,7 @@ const search = document.getElementById('query');
 const tip = document.getElementById('tip');
 
 var i = 0;
-var message = "now you'll go to error page";
+var message = "..";
 var typeSpeed = 200;
 
 
